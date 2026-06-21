e69ff3b2f9c6f94e8d8edc5ba15f0bd981b7b6fe
ahfurqan <afurqan05@gmail.com>
2026-06-21 22:42:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-.hidden{
-opacity:0;
-transform:translateY(40px);
-transition:all .8s ease;
-}
-
-.show{
-opacity:1;
-transform:translateY(0);
-}
-
-.active{
-color:#ffffff !important;
-font-weight:600;
-}
-
 const menuToggle =
 document.querySelector(".menu-toggle");
 
